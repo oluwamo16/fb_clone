6da73cf3613ceeabf60867113039fd400b64fee4
oluwamo16 <88567505+oluwamo16@users.noreply.github.com>
2022-03-27 23:53:10 +0100

    Update connectDb.js

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -16,7 +16,7 @@ async function dbConnect() {
 
 
 
- //const db = await mongoose.connect("mongodb+srv://shadrach:Shadrach16@cluster0.cofkx.mongodb.net/listad?retryWrites=true&w=majority", {
+ //const db = await mongoose.connect("mongodb+srv://shadrach:Shadrach16.com@cluster0.ousvj.mongodb.net/account?retryWrites=true&w=majority", {
 
 //const db = await mongoose.connect("mongodb://127.0.0.1:27017/", {
 
